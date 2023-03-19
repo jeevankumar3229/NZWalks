09f83d82c7ab0de7bb5fd6f6c16886c8e990e285
Jeevankumar <jeevankumar3229@gmail.com>
2023-03-19 10:19:44 +0530

    end of chapter2

---
+++ NZWalks-Api/Models/Domains/Region.cs
@@ -0,0 +1,24 @@
+﻿namespace NZWalks_Api.Models.Domains
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public long Population { get; set; }
+
+        //Navigation property- relationship between classes
+        //one region can have multiple walks
+
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
